a0eed3c41a83711e9e84a602c23b4234c4f9f00b
Seena George <seenageorge167@gmail.com>
2026-02-17 20:09:09 +0530

    Grand Child Class

---
+++ src/GrandChildClass.java
@@ -0,0 +1,14 @@
+public class GrandChildClass extends ChildClassOne{
+    GrandChildClass(String name) {
+        super(name);
+        System.out.println("Constructor"+name);
+    }
+
+    public static void main(String[] args){
+        ChildClassOne child1 = new ChildClassOne("Kaviraj");
+        System.out.println("Grandparent name: "+child1.name);
+
+        child1.whoami();
+        System.out.println(child1);
+    }
+}
